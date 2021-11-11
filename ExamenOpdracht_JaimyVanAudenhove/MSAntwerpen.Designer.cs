41827e8020acd3af62785a767ee493f2e80372a6
JaimyVA <jaimy2805ftw@gmail.com>
2021-11-11 11:13:27 +0100

    V1
    Application Version 1 DONE
    -Added Brussels/Antwerp Stores

--- ExamenOpdracht_JaimyVanAudenhove/MSAntwerpen.Designer.cs
+++ ExamenOpdracht_JaimyVanAudenhove/MSAntwerpen.Designer.cs
@@ -28,12 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.movieStoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.locatieSelectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.uitloggenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uitloggenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.examenDotNETAdvancedDataSet = new ExamenOpdracht_JaimyVanAudenhove.ExamenDotNETAdvancedDataSet();
+            this.examenDotNETAdvancedDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.moviesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.moviesTableAdapter = new ExamenOpdracht_JaimyVanAudenhove.ExamenDotNETAdvancedDataSetTableAdapters.MoviesTableAdapter();
+            this.ColumnMovieId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ratingDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rentalDurationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amountAntwerpenDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lenen = new System.Windows.Forms.DataGridViewButtonColumn();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.examenDotNETAdvancedDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.examenDotNETAdvancedDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviesBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -42,7 +58,7 @@
             this.movieStoreToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(744, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,13 +79,6 @@
             this.locatieSelectToolStripMenuItem.Text = "Terug naar Locatie select";
             this.locatieSelectToolStripMenuItem.Click += new System.EventHandler(this.locatieSelectToolStripMenuItem_Click);
             // 
-            // uitloggenToolStripMenuItem
-            // 
-            this.uitloggenToolStripMenuItem.Name = "uitloggenToolStripMenuItem";
-            this.uitloggenToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
-            this.uitloggenToolStripMenuItem.Text = "Uitloggen";
-            this.uitloggenToolStripMenuItem.Click += new System.EventHandler(this.uitloggenToolStripMenuItem_Click);
-            // 
             // accountToolStripMenuItem
             // 
             this.accountToolStripMenuItem.Name = "accountToolStripMenuItem";
@@ -77,17 +86,111 @@
             this.accountToolStripMenuItem.Text = "Account";
             this.accountToolStripMenuItem.Click += new System.EventHandler(this.accountToolStripMenuItem_Click);
             // 
+            // uitloggenToolStripMenuItem
+            // 
+            this.uitloggenToolStripMenuItem.Name = "uitloggenToolStripMenuItem";
+            this.uitloggenToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.uitloggenToolStripMenuItem.Text = "Uitloggen";
+            this.uitloggenToolStripMenuItem.Click += new System.EventHandler(this.uitloggenToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnMovieId,
+            this.nameDataGridViewTextBoxColumn,
+            this.ratingDataGridViewTextBoxColumn,
+            this.rentalDurationDataGridViewTextBoxColumn,
+            this.amountAntwerpenDataGridViewTextBoxColumn,
+            this.Lenen});
+            this.dataGridView1.DataSource = this.moviesBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(744, 426);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // examenDotNETAdvancedDataSet
+            // 
+            this.examenDotNETAdvancedDataSet.DataSetName = "ExamenDotNETAdvancedDataSet";
+            this.examenDotNETAdvancedDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // examenDotNETAdvancedDataSetBindingSource
+            // 
+            this.examenDotNETAdvancedDataSetBindingSource.DataSource = this.examenDotNETAdvancedDataSet;
+            this.examenDotNETAdvancedDataSetBindingSource.Position = 0;
+            // 
+            // moviesBindingSource
+            // 
+            this.moviesBindingSource.DataMember = "Movies";
+            this.moviesBindingSource.DataSource = this.examenDotNETAdvancedDataSetBindingSource;
+            // 
+            // moviesTableAdapter
+            // 
+            this.moviesTableAdapter.ClearBeforeFill = true;
+            // 
+            // ColumnMovieId
+            // 
+            this.ColumnMovieId.DataPropertyName = "MovieId";
+            this.ColumnMovieId.HeaderText = "Film ID";
+            this.ColumnMovieId.Name = "ColumnMovieId";
+            this.ColumnMovieId.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Naam";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // ratingDataGridViewTextBoxColumn
+            // 
+            this.ratingDataGridViewTextBoxColumn.DataPropertyName = "Rating";
+            this.ratingDataGridViewTextBoxColumn.HeaderText = "Minimum Leeftijd";
+            this.ratingDataGridViewTextBoxColumn.Name = "ratingDataGridViewTextBoxColumn";
+            // 
+            // rentalDurationDataGridViewTextBoxColumn
+            // 
+            this.rentalDurationDataGridViewTextBoxColumn.DataPropertyName = "Rental_Duration";
+            this.rentalDurationDataGridViewTextBoxColumn.HeaderText = "Aantal dagen leentijd";
+            this.rentalDurationDataGridViewTextBoxColumn.Name = "rentalDurationDataGridViewTextBoxColumn";
+            // 
+            // amountAntwerpenDataGridViewTextBoxColumn
+            // 
+            this.amountAntwerpenDataGridViewTextBoxColumn.DataPropertyName = "Amount_Antwerpen";
+            this.amountAntwerpenDataGridViewTextBoxColumn.HeaderText = "Aantal Beschikbaar";
+            this.amountAntwerpenDataGridViewTextBoxColumn.Name = "amountAntwerpenDataGridViewTextBoxColumn";
+            // 
+            // Lenen
+            // 
+            this.Lenen.HeaderText = "Deze Lenen";
+            this.Lenen.Name = "Lenen";
+            this.Lenen.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Lenen.Text = "Lenen";
+            // 
             // MSAntwerpen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MSAntwerpen";
-            this.Text = "MSAntwerpen";
+            this.Text = "MovieStore Antwerpen";
+            this.Load += new System.EventHandler(this.MSAntwerpen_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.examenDotNETAdvancedDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.examenDotNETAdvancedDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviesBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,5 +203,16 @@
         private System.Windows.Forms.ToolStripMenuItem locatieSelectToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem uitloggenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem accountToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource examenDotNETAdvancedDataSetBindingSource;
+        private ExamenDotNETAdvancedDataSet examenDotNETAdvancedDataSet;
+        private System.Windows.Forms.BindingSource moviesBindingSource;
+        private ExamenDotNETAdvancedDataSetTableAdapters.MoviesTableAdapter moviesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMovieId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ratingDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rentalDurationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amountAntwerpenDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Lenen;
     }
 }
--- ExamenOpdracht_JaimyVanAudenhove/MSAntwerpen.cs
+++ ExamenOpdracht_JaimyVanAudenhove/MSAntwerpen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,12 @@ namespace ExamenOpdracht_JaimyVanAudenhove
 {
     public partial class MSAntwerpen : Form
     {
+        private const string ConnectionString = @"Data Source=jaimy.database.windows.net;Initial Catalog=ExamenDotNETAdvanced;Persist Security Info=True;User ID=jaimy;Password=DotNetExamen1;MultipleActiveResultSets=true";
+        private SqlCommand cmd;
+        private SqlDataReader dr;
+        private SqlConnection cn;
+        DateTime today = DateTime.Today;
+
         public MSAntwerpen()
         {
             InitializeComponent();
@@ -36,6 +43,185 @@ namespace ExamenOpdracht_JaimyVanAudenhove
             var HomescreenToAccount = new Account();
             this.Hide();
             HomescreenToAccount.Show();
+        }
+
+        private void MSAntwerpen_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'examenDotNETAdvancedDataSet.Movies' table. You can move, or remove it, as needed.
+            this.moviesTableAdapter.Fill(this.examenDotNETAdvancedDataSet.Movies);
+            //TODO: This line of code loads data into the 'examenDotNETAdvancedDataSet.Movies' table.You can move, or remove it, as needed.
+            //    this.moviesTableAdapter.Fill(this.examenDotNETAdvancedDataSet.Movies);
+            //TODO: This line of code loads data into the 'examenDotNETAdvancedDataSet.Users' table.You can move, or remove it, as needed.
+            //    this.usersTableAdapter.Fill(this.examenDotNETAdvancedDataSet.Users);
+
+            cn = new SqlConnection(@"Data Source=jaimy.database.windows.net;Initial Catalog=ExamenDotNETAdvanced;Persist Security Info=True;User ID=jaimy;Password=DotNetExamen1;MultipleActiveResultSets=true");
+            cn.Open();
+
+            BindGrid();
+
+        }
+        private void BindGrid()
+        {
+            //Hide the last blank line.
+            dataGridView1.AllowUserToAddRows = false;
+
+            //Clear Columns.
+            dataGridView1.Columns.Clear();
+
+            //Add Columns.
+            DataGridViewColumn filmId = new DataGridViewTextBoxColumn();
+            filmId.Name = "filmId";
+            filmId.HeaderText = "Film ID";
+            filmId.DataPropertyName = "MovieId";
+            filmId.Width = 100;
+            dataGridView1.Columns.Insert(0, filmId);
+
+            DataGridViewColumn Naam = new DataGridViewTextBoxColumn();
+            Naam.HeaderText = "Naam";
+            Naam.Name = "Naam";
+            Naam.DataPropertyName = "Name";
+            Naam.Width = 200;
+            dataGridView1.Columns.Insert(1, Naam);
+
+            DataGridViewColumn MinLeeftijd = new DataGridViewTextBoxColumn();
+            MinLeeftijd.Name = "MinLeeftijd";
+            MinLeeftijd.HeaderText = "Minimum Leeftijd";
+            MinLeeftijd.DataPropertyName = "Rating";
+            MinLeeftijd.Width = 100;
+            dataGridView1.Columns.Insert(2, MinLeeftijd);
+
+            DataGridViewColumn Leentijd = new DataGridViewTextBoxColumn();
+            Leentijd.Name = "Leentijd";
+            Leentijd.HeaderText = "Aantal dagen leentijd";
+            Leentijd.DataPropertyName = "Rental_Duration";
+            Leentijd.Width = 100;
+            dataGridView1.Columns.Insert(3, Leentijd);
+
+            DataGridViewColumn AantalBeschikbaar = new DataGridViewTextBoxColumn();
+            AantalBeschikbaar.Name = "AantalBeschikbaar";
+            AantalBeschikbaar.HeaderText = "Aantal Beschikbaar";
+            AantalBeschikbaar.DataPropertyName = "Amount_Antwerpen";
+            AantalBeschikbaar.Width = 100;
+            dataGridView1.Columns.Insert(4, AantalBeschikbaar);
+
+            //Bind the DataGridView.
+            dataGridView1.DataSource = null;
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT MovieId, Name, Rating, Rental_Duration, Amount_Antwerpen FROM Movies", con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+            }
+
+            //Add the Button Column.
+            DataGridViewButtonColumn Lenen = new DataGridViewButtonColumn();
+            Lenen.HeaderText = "Deze Film Lenen";
+            Lenen.Width = 100;
+            Lenen.Name = "Lenen";
+            Lenen.Text = "Lenen";
+            Lenen.UseColumnTextForButtonValue = true;
+            dataGridView1.Columns.Insert(5, Lenen);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            var FirstCell = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            var MovieNameCell = Convert.ToString(dataGridView1.CurrentRow.Cells[1].Value);
+            var RatingCell = Convert.ToInt32(dataGridView1.CurrentRow.Cells[2].Value);
+            var AantalBeschikbaarCell = Convert.ToInt32(dataGridView1.CurrentRow.Cells[4].Value);
+            var LeentijdCell = Convert.ToInt32(dataGridView1.CurrentRow.Cells[3].Value);
+
+            if (AantalBeschikbaarCell > 0)
+            {
+                //Check User strikes
+                using (SqlCommand sqlCommand = new SqlCommand("select Strikes from Users where username='" + MSLogin.SetValueForUserName + "'", cn))
+                {
+                    sqlCommand.CommandType = CommandType.Text;
+                    sqlCommand.Connection = cn;
+                    using (SqlDataReader sdr = sqlCommand.ExecuteReader())
+                    {
+                        sdr.Read();
+                        int UserStrikes = (int)sdr["Strikes"];
+                        if (UserStrikes >= 3)
+                        {
+                            MessageBox.Show("Je bent verbannen van de MovieStore omdat je meermaals films niet op tijd terugbracht!");
+                        }
+                        else
+                        {
+                            //Check if user is currently renting a movie
+                            using (SqlCommand sqlCommandCurrentlyRenting = new SqlCommand("select * from Rental where username='" + MSLogin.SetValueForUserName + "'", cn))
+                            {
+                                sqlCommandCurrentlyRenting.CommandType = CommandType.Text;
+                                sqlCommandCurrentlyRenting.Connection = cn;
+                                using (SqlDataReader sdrCurrentlyRenting = sqlCommandCurrentlyRenting.ExecuteReader())
+                                {
+                                    if (sdrCurrentlyRenting.Read())
+                                    {
+                                        sdrCurrentlyRenting.Close();
+                                        MessageBox.Show("Je leent al een film, breng deze eerst terug!");
+                                    }
+                                    else
+                                    {
+                                        sdrCurrentlyRenting.Close();
+                                        //user DOB Query
+                                        using (SqlCommand sqlCommand1 = new SqlCommand("select DOB from Users where username='" + MSLogin.SetValueForUserName + "'", cn))
+                                        {
+                                            sqlCommand1.CommandType = CommandType.Text;
+                                            sqlCommand1.Connection = cn;
+                                            using (SqlDataReader sdr1 = sqlCommand1.ExecuteReader())
+                                            {
+                                                sdr1.Read();
+                                                DateTime dob = (DateTime)sdr1["DOB"];
+                                                if (today.Year - dob.Year > RatingCell)
+                                                {
+                                                    //Movie update query
+                                                    cmd = new SqlCommand("select * from Movies where Name='" + FirstCell + "'", cn);
+                                                    dr = cmd.ExecuteReader();
+                                                    dr.Close();
+                                                    cmd = new SqlCommand("UPDATE Movies SET Amount_Antwerpen = Amount_Antwerpen - 1 WHERE MovieId ='" + FirstCell + "'", cn);
+                                                    cmd.Parameters.AddWithValue("Amount_Antwerpen", AantalBeschikbaarCell - 1);
+                                                    cmd.ExecuteNonQuery();
+                                                    MessageBox.Show("Je hebt " + MovieNameCell + " Geleend! Breng de film zeker op tijd terug!", MovieNameCell + " Geleend", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                                    //Rental query
+                                                    cmd = new SqlCommand("insert into Rental values(@Rental_Date,@Rental_Expiry,@username)", cn);
+                                                    cmd.Parameters.AddWithValue("Rental_Date", today.ToString("yyyy-MM-dd"));
+                                                    cmd.Parameters.AddWithValue("Rental_Expiry", today.AddDays(LeentijdCell));
+                                                    cmd.Parameters.AddWithValue("username", MSLogin.SetValueForUserName);
+                                                    cmd.ExecuteNonQuery();
+                                                    //Movie_Rentals query
+                                                    cmd = new SqlCommand("insert into Movie_Rentals values(@MovieId)", cn);
+                                                    cmd.Parameters.AddWithValue("MovieId", FirstCell);
+                                                    cmd.ExecuteNonQuery();
+                                                }
+                                                else
+                                                {
+                                                    MessageBox.Show("Je bent te jong voor deze film");
+                                                }
+                                            }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Deze Film is momenteel niet meer beschikbaar");
+            }
+            this.BindGrid();
         }
     }
 }
